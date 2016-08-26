e010147e21ccb7d768a64c89dde7fa97d62a15c7
Rohin Gosling <rohingosling@users.noreply.github.com>
2016-08-26 12:06:12 +0200

    Formatting: Fixed tab and space alignment.

--- neural_network/neural_network_layer.h
+++ neural_network/neural_network_layer.h
@@ -70,10 +70,10 @@ class NeuralNetworkLayer
 public:
 
 	const string C_EMPTY          = "";
-    const string C_SPACE          = " ";
+        const string C_SPACE          = " ";
 	const string C_NEW_LINE       = "\n";
-    const string C_TAB_1          = "\t";
-    const string C_TAB_2          = "\t\t";   
+        const string C_TAB_1          = "\t";
+        const string C_TAB_2          = "\t\t";   
 	const char   C_DIGIT_LEFT_PAD = ' ';   
 	const char   C_LABEL_LEFT_PAD = ' ';   
 
@@ -83,7 +83,7 @@ public:
 		
 	const int  C_DEFAULT_WEIGHT_PRECISION           = 3; 
 	const int  C_DEFAULT_BIAS_PRECISION             = 3; 
-    const int  C_DEFAULT_CONSOLE_DATA_PRECISION     = 3; 
+        const int  C_DEFAULT_CONSOLE_DATA_PRECISION     = 3; 
 	const int  C_DEFAULT_CONSOLE_DATA_WIDTH         = 7;
 	const bool C_DEFAULT_CONSOLE_HEADER_ROW_ENABLED = true;
 	const int  C_DEFAULT_DSV_PRECISION              = 0;
@@ -105,20 +105,20 @@ public:
 	using TransferFunctionType = enum
 	{
 		LINEAR,     // Direct weight function + bias.   y = x
-        STEP,		// Step function.                   y = step(x)
-        SGN,        // Sign function.                   y = sgn(x)
-        SIGMOID,    // Sigmoid logistics function.      y = sig(x)
-        TANH,       // Hyperbolic tangent.              y = tanh(x)
-        TANH2		// Steep hyperbolic tangent.        y = tanh(2x)
+                STEP,       // Step function.                   y = step(x)
+                SGN,        // Sign function.                   y = sgn(x)
+                SIGMOID,    // Sigmoid logistics function.      y = sig(x)
+                TANH,       // Hyperbolic tangent.              y = tanh(x)
+                TANH2       // Steep hyperbolic tangent.        y = tanh(2x)
 	};
 
 	using BinaryFileHeader = struct
-	{
-		char     file_type_name[16];						//  0 16: Set to "ANN Layer File.\n".
-		char     zero_terminator;							// 16  1: Set to zero. e.g. 0x00.
-		uint8_t  data_width;								// 17  1: 8, 16, 32, 64, 128, 255.
-		uint32_t input_unit_count;							// 18  4: Number o finput units.
-		uint32_t output_unit_count;							// 22  4: Number of output units.
+	 {
+		char     file_type_name[16];                            //  0 16: Set to "ANN Layer File.\n".
+		char     zero_terminator;                               // 16  1: Set to zero. e.g. 0x00.
+		uint8_t  data_width;	                                // 17  1: 8, 16, 32, 64, 128, 255.
+		uint32_t input_unit_count;                              // 18  4: Number o finput units.
+		uint32_t output_unit_count;                             // 22  4: Number of output units.
 		uint8_t  reserved [ C_BIN_FILE_RESERVED_SPACE ];	// 26  6: Reserved space.
 	};
 
@@ -130,17 +130,17 @@ public:
 
 	// Constructors and destructors
 
-    NeuralNetworkLayer  ( void );
+        NeuralNetworkLayer  ( void );
 	NeuralNetworkLayer  ( int input_unit_count, int output_unit_count );
-    ~NeuralNetworkLayer ( void );
+        ~NeuralNetworkLayer ( void );
 
 	// Class managment functions.
 
 	void initialize ( int input_unit_count, int output_unit_count );
 	
-    // Nerual network layer functions.
+        // Nerual network layer functions.
 
-    void            set_weights           ( double weight );
+        void            set_weights           ( double weight );
 	void            randomize_weights     ( double min, double max );
 	void            gaussian_weights      ( double min, double max );
 	void            setbias_vector        ( double bias_term );
